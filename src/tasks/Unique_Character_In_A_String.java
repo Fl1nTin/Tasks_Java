2a767419871221b038f8a94c7e429ed241ebd7b5
Fl1nTin <1998mill@mail.ru>
2022-09-26 16:09:16 +0300

    Initial commit

---
+++ src/tasks/Unique_Character_In_A_String.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Напишите функцию, которая определяет уникальны ли все символы в строке. Регистр должен учитываться
+
+public class Unique_Character_In_A_String {
+    public static boolean isUnique(String str){
+        Set<Character> list = new HashSet<>();
+        for (int i = 0; i < str.length(); i++)
+            list.add(str.charAt(i));
+
+        return list.size() == str.length();
+    }
+}
