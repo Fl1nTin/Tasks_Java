3daae25d28549efd968e417316dec3fd2b21b93c
Fl1nTin <1998mill@mail.ru>
2023-02-07 19:01:51 +0300

    Initial commit

---
+++ src/tasks/Spin_Words.java
@@ -0,0 +1,15 @@
+package tasks;
+
+/*Напишите функцию, которая принимает строку из одного или нескольких слов и возвращает ту же строку,
+но со всеми пятью или более буквенными словами наоборот.
+Передаваемые строки будут состоять только из букв и пробелов.
+Пробелы будут включены только в том случае, если присутствует более одного слова.*/
+public class Spin_Words {
+    public static String spinWords(String sentence) {
+        for (String i : sentence.split(" ")) {
+            if (i.length() > 4)
+                sentence = sentence.replace(i, new StringBuilder(i).reverse());
+        }
+        return sentence;
+    }
+}
